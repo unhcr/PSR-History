3e0d6a67c0e4f9a6be3c40992fba3d85d0c9502d
Peter Scott <scott@unhcr.org>
2012-01-11 15:52:47 +0000

    Allow notes to be associated with messages. Tidy up comments.

--- SQL/InitPopulate.sql
+++ SQL/InitPopulate.sql
@@ -1,11 +1,14 @@
 -- Table aliases
+-- =============
 
 insert into TABLE_ALIASES (ALIAS, TABLE_NAME)
 select substr(CONSTRAINT_NAME, 4), TABLE_NAME
 from USER_CONSTRAINTS
 where CONSTRAINT_TYPE = 'P';
 
+
 -- Initial "bootstrapping" entries in text-related tables
+-- ======================================================
 
 variable TXT_ID number;
 insert into TEXT_HEADERS (ID, TAB_ALIAS) values (TXT_SEQ.nextval, 'TXTT') returning ID into :TXT_ID;
@@ -19,7 +22,9 @@ insert into LANGUAGES (CODE, TXT_ID) values ('en', TXT_SEQ.currval);
 insert into TEXT_ITEMS (TXT_ID, TXTT_CODE, SEQ_NBR, LANG_CODE, TEXT) values (:TXT_ID, 'DESCR', 1, 'en', 'Description');
 insert into TEXT_ITEMS (TXT_ID, TXTT_CODE, SEQ_NBR, LANG_CODE, TEXT) values (TXT_SEQ.currval, 'DESCR', 1, 'en', 'English');
 
--- Language records
+
+-- Languages
+-- =========
 
 -- All the UN languages
 execute LANGUAGE.INSERT_LANGUAGE('fr', 'en', 'French');
@@ -29,6 +34,7 @@ execute LANGUAGE.INSERT_LANGUAGE('ar', 'en', 'Arabic');
 execute LANGUAGE.INSERT_LANGUAGE('zh', 'en', 'Chinese');
 
 -- Descriptions in all the UN languages
+
 execute LANGUAGE.ADD_LANG_DESCRIPTION('en', 'fr', unistr('Anglais'));
 execute LANGUAGE.ADD_LANG_DESCRIPTION('en', 'es', unistr('Ingl\00E9s'));
 execute LANGUAGE.ADD_LANG_DESCRIPTION('en', 'ru', unistr('\0430\043D\0433\043B\0438\0439\0441\043A\0438\0439'));
@@ -66,41 +72,45 @@ execute LANGUAGE.ADD_LANG_DESCRIPTION('zh', 'ar', unistr('\0627\0644\0635\064A\0
 execute LANGUAGE.ADD_LANG_DESCRIPTION('zh', 'zh', unistr('\4E2D\56FD'));
 
 -- Inactive language for testing (English description only)
+
 execute LANGUAGE.INSERT_LANGUAGE('la', 'en', 'Latin (inactive language for testing)', psACTIVE_FLAG => 'N');
 
--- Text types with descriptions in all UN languages
 
--- Description text type properties for all relevant tables
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS, MANDATORY, MULTI_INSTANCE) values ('DESCR', 'COMP', 'Y', 'N');
+-- Text types with descriptions in all UN languages and text type properties
+-- =========================================================================
+
+-- Description text type properties and description descriptions
+
 execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('DESCR', 'COMP', 'Y', 'N');
 
--- Descriptions descriptions
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('DESCR', 'fr', unistr('Description'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('DESCR', 'es', unistr('Descripci\00F3n'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('DESCR', 'ru', unistr('\043E\043F\0438\0441\0430\043D\0438\0435'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('DESCR', 'ar', unistr('\0648\0635\0641'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('DESCR', 'zh', unistr('\8BF4\660E'));
 
--- Messages and messages descriptions
+-- Messages, message text type properties, and message descriptions
+
 execute TEXT_TYPE.INSERT_TEXT_TYPE('MSG', 'en', 'Message');
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS, MANDATORY, MULTI_INSTANCE) values ('MSG', 'MSG', 'Y', 'N');
+
 execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('MSG', 'MSG', 'Y', 'N');
+
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('MSG', 'fr', unistr('Message'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('MSG', 'es', unistr('Mensaje'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('MSG', 'ru', unistr('\0441\043E\043E\0431\0449\0435\043D\0438\0435'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('MSG', 'ar', unistr('\0631\0633\0627\0644\0629'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('MSG', 'zh', unistr('\6D88\606F'));
 
--- Notes and notes descriptions
+-- Notes, note text type properties, and note descriptions
+
 execute TEXT_TYPE.INSERT_TEXT_TYPE('NOTE', 'en', 'Note');
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS) values ('NOTE', 'TAB');
+
+execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'TAB');
+execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'TXTT');
+execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'LANG');
 execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'COMP');
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS) values ('NOTE', 'TXTT');
-execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'COMP');
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS) values ('NOTE', 'LANG');
-execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'COMP');
--- insert into TEXT_TYPE_PROPERTIES (TXTT_CODE, TAB_ALIAS) values ('NOTE', 'COMP');
-execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'COMP');
+execute TEXT_TYPE.SET_TEXT_TYPE_PROPERTIES('NOTE', 'MSG');
+
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('NOTE', 'fr', unistr('Remarque'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('NOTE', 'es', unistr('Nota'));
 execute TEXT_TYPE.ADD_TXTT_DESCRIPTION('NOTE', 'ru', unistr('\043F\0440\0438\043C\0435\0447\0430\043D\0438\0435'));
