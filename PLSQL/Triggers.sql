47df0064832064cb720fb0d83749c062bc1bc328
Peter Scott <scott@unhcr.org>
2012-01-11 15:39:44 +0000

    Remove trigger TXTT_RBD_TEXT to avoid mutating table problem.

--- PLSQL/Triggers.sql
+++ PLSQL/Triggers.sql
@@ -1,11 +1,3 @@
-create or replace trigger TXTT_RBD_TEXT
-before delete on TEXT_TYPES
-for each row
-begin
-  TEXT.DELETE_TEXT(:old.TXT_ID);
-end;
-/
-
 create or replace trigger LANG_RBD_TEXT
 before delete on LANGUAGES
 for each row
