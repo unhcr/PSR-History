8ef81f85831ae5de871c53fd048431b5d5617fd4
Peter Scott <scott@unhcr.org>
2012-01-13 14:51:32 +0000

    Add test cases for SET_TEXT and additional cases for DELETE_TEXT.

--- PLSQL/test_TEXT.sql
+++ PLSQL/test_TEXT.sql
@@ -3,18 +3,89 @@
 
 define eh = "exception when others then dbms_output.put_line(substr(sqlerrm, case when sqlcode = -20009 then 12 else 1 end))"
 
-var TXT_ID number
-var TXT_ID1 number
-var TXT_ID2 number
-var SEQ_NBR number
-var SEQ_NBR1 number
-var SEQ_NBR2 number
+variable TXT_ID number
+variable TXT_ID1 number
+variable TXT_ID2 number
+variable SEQ_NBR number
+variable SEQ_NBR1 number
+variable SEQ_NBR2 number
 
 execute TEXT_TYPE.INSERT_TEXT_TYPE('INACT', 'en', 'Inactive', null, 'N');
 
 set echo on serveroutput on feedback off
 
 spool test_TEXT.log
+
+-- Set text
+-- ========
+
+-- Success cases
+-- -------------
+
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', 'English note 1'); :TXT_ID1 := :TXT_ID; &eh
+execute :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note 2'); :SEQ_NBR1 := :SEQ_NBR; &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'fr', 'French note'); &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'es', rpad('x', 1001, unistr('\00F1'))); &eh
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'DESCR', :SEQ_NBR, 'en', 'English description'); :TXT_ID2 := :TXT_ID; :SEQ_NBR2 := :SEQ_NBR; &eh
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := :SEQ_NBR1; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', 'English note 2a'); &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note 2b'); &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'es', 'Spanish note 3'); &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'fr', rpad('French note ', 1001, unistr('\8BF4'))); &eh
+execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'fr', rpad('French note ', 1001, '$')); &eh
+
+-- Error cases
+-- -----------
+
+-- cannot insert NULL into ("PSR"."TEXT_HEADERS"."TAB_ALIAS")
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Cannot specify a text item sequence number without a text identifier
+execute :TXT_ID := null; :SEQ_NBR := 1; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- integrity constraint (PSR.FK_TXT_TAB) violated - parent key not found
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'XXXX', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Unknown text type
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'XXXX', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Inactive text type
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'INACT', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'MSG', :SEQ_NBR, 'en', 'English message'); &eh
+
+-- no data found
+execute :TXT_ID := 9999999; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'MSG', :SEQ_NBR, 'en', 'English message'); &eh
+
+-- Wrong table for this text identifier
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'TXTT', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Unknown text type
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'XXXX', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Inactive text type
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'INACT', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Only one text item of this type allowed
+execute :TXT_ID := :TXT_ID2; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'DESCR', :SEQ_NBR, 'en', 'English description'); &eh
+
+-- No existing text of this type
+execute :TXT_ID := :TXT_ID2; :SEQ_NBR := :SEQ_NBR2; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Text item sequence number greater than current maximum
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := 9; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
+
+-- Unknown text language
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'de', 'German note'); &eh
+
+-- Inactive text language
+execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'la', 'Latin note'); &eh
+
+-- check constraint (PSR.CH_TXI_TEXT) violated
+execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', ''); &eh
 
 -- Insert text
 -- ===========
@@ -49,7 +120,7 @@ execute :TXT_ID := null; :SEQ_NBR := null; TEXT.INSERT_TEXT(:TXT_ID, 'LANG', 'IN
 -- integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.INSERT_TEXT(:TXT_ID, 'LANG', 'MSG', :SEQ_NBR, 'en', 'English message'); &eh
 
--- Unknown text identifier
+-- no data found
 execute :TXT_ID := 9999999; :SEQ_NBR := null; TEXT.INSERT_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
 
 -- integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
@@ -140,6 +211,36 @@ execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR', :SEQ_NBR2); &eh
 
 -- Cannot delete mandatory text type
 execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(:TXT_ID1, 'NOTE', null, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(:TXT_ID1, null, :SEQ_NBR1, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(:TXT_ID1, null, null, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, 'NOTE', :SEQ_NBR1, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, 'NOTE', null, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, null, :SEQ_NBR1, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, null, null, 'fr'); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(:TXT_ID1, null, :SEQ_NBR1); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, 'NOTE', :SEQ_NBR1); &eh
+
+-- No text to delete
+execute TEXT.DELETE_TEXT(null, null, :SEQ_NBR1); &eh
 
 -- Success cases
 -- -------------
