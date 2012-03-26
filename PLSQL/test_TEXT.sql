2905c1e8e282f47b57062926d93a89e55473b58e
Peter Scott <scott@unhcr.org>
2012-03-26 09:15:23 +0000

    Change presentation layout of error messages. Display audit column details.

--- PLSQL/test_TEXT.sql
+++ PLSQL/test_TEXT.sql
@@ -1,7 +1,7 @@
 -- Test set-up
 -- ===========
 
-define eh = "exception when others then dbms_output.put_line(substr(sqlerrm, case when substr(sqlerrm, 12, 5) = 'ORA-2' then 12 else 1 end))"
+define eh = "exception when others then dbms_output.put_line('-- ' || sqlerrm)"
 
 variable TXT_ID number
 variable TXT_ID1 number
@@ -10,11 +10,12 @@ variable SEQ_NBR number
 variable SEQ_NBR1 number
 variable SEQ_NBR2 number
 
-execute TEXT_TYPE.SET_TEXT_TYPE('INACT', 'en', 'Inactive', null, 'N');
+execute TEXT_TYPE.INSERT_TEXT_TYPE('INACT', 'en', 'Inactive', null, 'N');
 
-set echo on serveroutput on feedback off recsepchar "." sqlprompt "        " sqlnumber off
+set echo on serveroutput on feedback off recsepchar "." sqlprompt "" sqlnumber off
 
 column LONG_TEXT format A150
+column UPDATE_TIMESTAMP format A25
 
 spool test_TEXT.log
 
@@ -37,7 +38,7 @@ execute TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'fr', rpad('French note '
 
 select TXT.ID, TXT.TAB_ALIAS,
   TTH.TXTT_CODE, TTH.TAB_ALIAS, TTH.TXI_SEQ_NBR_MAX,
-  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TEXT, TXI.LONG_TEXT
+  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TAB_ALIAS, TXI.UPDATE_TIMESTAMP, TXI.UPDATE_USERID, TXI.TEXT, TXI.LONG_TEXT
 from TEXT_HEADERS TXT
 join TEXT_TYPE_HEADERS TTH
   on TTH.TXT_ID = TXT.ID
@@ -54,16 +55,16 @@ execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', 
 -- ORA-01400: cannot insert NULL into ("PSR"."TEXT_HEADERS"."TAB_ALIAS")
 
 execute :TXT_ID := null; :SEQ_NBR := 1; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0006: Cannot specify a text item sequence number without a text identifier
+-- ORA-20002: TXT-0005: Cannot specify a text item sequence number without a text identifier
 
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'XXXX', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
 -- ORA-02291: integrity constraint (PSR.FK_TXT_TAB) violated - parent key not found
 
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'XXXX', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0002: Unknown text type
+-- ORA-20002: TXT-0001: Unknown text type
 
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'INACT', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0003: Inactive text type
+-- ORA-20002: TXT-0002: Inactive text type
 
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'MSG', :SEQ_NBR, 'en', 'English message'); &eh
 -- ORA-02291: integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
@@ -75,16 +76,16 @@ execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'M
 -- ORA-02291: integrity constraint (PSR.FK_TTH_TTP) violated - parent key not found
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'TXTT', 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0007: Wrong table for this text identifier
+-- ORA-20002: TXT-0006: Wrong table for this text identifier
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'XXXX', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0002: Unknown text type
+-- ORA-20002: TXT-0001: Unknown text type
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'INACT', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0003: Inactive text type
+-- ORA-20002: TXT-0002: Inactive text type
 
 execute :TXT_ID := :TXT_ID2; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'DESCR', :SEQ_NBR, 'en', 'English description'); &eh
--- TXT-0008: Only one text item of this type allowed
+-- ORA-20002: TXT-0007: Only one text item of this type allowed
 
 execute :TXT_ID := :TXT_ID2; :SEQ_NBR := :SEQ_NBR2; TEXT.SET_TEXT(:TXT_ID, null, 'DESCR', :SEQ_NBR, 'en', rpad('English description ', 1001, '$')); &eh
 -- ORA-12899: value too large for column "PSR"."TEXT_ITEMS"."TEXT" (actual: 1001, maximum: 1000)
@@ -93,23 +94,23 @@ execute :TXT_ID := :TXT_ID2; :SEQ_NBR := :SEQ_NBR2; TEXT.SET_TEXT(:TXT_ID, null,
 -- ORA-12899: value too large for column "PSR"."TEXT_ITEMS"."TEXT" (actual: 1001, maximum: 1000)
 
 execute :TXT_ID := :TXT_ID2; :SEQ_NBR := :SEQ_NBR2; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0009: No existing text of this type
+-- ORA-20002: TXT-0008: No existing text of this type
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := 9; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'en', 'English note'); &eh
--- TXT-0010: Text item sequence number greater than current maximum
+-- ORA-20002: TXT-0009: Text item sequence number greater than current maximum
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'de', 'German note'); &eh
--- TXT-0004: Unknown text language
+-- ORA-20002: TXT-0003: Unknown text language
 
 execute :TXT_ID := :TXT_ID1; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, null, 'NOTE', :SEQ_NBR, 'la', 'Latin note'); &eh
--- TXT-0005: Inactive text language
+-- ORA-20002: TXT-0004: Inactive text language
 
 execute :TXT_ID := null; :SEQ_NBR := null; TEXT.SET_TEXT(:TXT_ID, 'LANG', 'NOTE', :SEQ_NBR, 'en', ''); &eh
--- TXT-0001: Text must be specified
+-- ORA-06502: PL/SQL: numeric or value error
 
 select TXT.ID, TXT.TAB_ALIAS,
   TTH.TXTT_CODE, TTH.TAB_ALIAS, TTH.TXI_SEQ_NBR_MAX,
-  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TEXT, TXI.LONG_TEXT
+  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TAB_ALIAS, TXI.UPDATE_TIMESTAMP, TXI.UPDATE_USERID, TXI.TEXT, TXI.LONG_TEXT
 from TEXT_HEADERS TXT
 join TEXT_TYPE_HEADERS TTH
   on TTH.TXT_ID = TXT.ID
@@ -126,34 +127,34 @@ order by TXT.ID, TTH.TXTT_CODE, TXI.SEQ_NBR, TXI.LANG_CODE;
 -- -----------
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, 'NOTE', :SEQ_NBR1, 'de'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, 'NOTE', 9999); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, 'XXXX'); &eh
 -- ORA-01403: no data found
 
 execute TEXT.DELETE_TEXT(9999999); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR', :SEQ_NBR2, 'en'); &eh
--- TXT-0012: Cannot delete last mandatory text item
+-- ORA-20002: TXT-0011: Cannot delete last mandatory text item
 
 execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR', :SEQ_NBR2); &eh
--- TXT-0012: Cannot delete last mandatory text item
+-- ORA-20002: TXT-0011: Cannot delete last mandatory text item
 
 execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR'); &eh
--- TXT-0013: Cannot delete mandatory text type
+-- ORA-20002: TXT-0012: Cannot delete mandatory text type
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, 'NOTE', null, 'fr'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, null, :SEQ_NBR1, 'fr'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, null, null, 'fr'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(null, 'NOTE', :SEQ_NBR1, 'fr'); &eh
 -- ORA-01403: no data found
@@ -162,23 +163,23 @@ execute TEXT.DELETE_TEXT(null, 'NOTE', null, 'fr'); &eh
 -- ORA-01403: no data found
 
 execute TEXT.DELETE_TEXT(null, null, :SEQ_NBR1, 'fr'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(null, null, null, 'fr'); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(:TXT_ID1, null, :SEQ_NBR1); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 execute TEXT.DELETE_TEXT(null, 'NOTE', :SEQ_NBR1); &eh
 -- ORA-01403: no data found
 
 execute TEXT.DELETE_TEXT(null, null, :SEQ_NBR1); &eh
--- TXT-0011: No text to delete
+-- ORA-20002: TXT-0010: No text to delete
 
 select TXT.ID, TXT.TAB_ALIAS,
   TTH.TXTT_CODE, TTH.TAB_ALIAS, TTH.TXI_SEQ_NBR_MAX,
-  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TEXT, TXI.LONG_TEXT
+  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TAB_ALIAS, TXI.UPDATE_TIMESTAMP, TXI.UPDATE_USERID, TXI.TEXT, TXI.LONG_TEXT
 from TEXT_HEADERS TXT
 join TEXT_TYPE_HEADERS TTH
   on TTH.TXT_ID = TXT.ID
@@ -206,7 +207,7 @@ execute TEXT.DELETE_TEXT(:TXT_ID2, 'DESCR', :SEQ_NBR2, 'en'); &eh
 
 select TXT.ID, TXT.TAB_ALIAS,
   TTH.TXTT_CODE, TTH.TAB_ALIAS, TTH.TXI_SEQ_NBR_MAX,
-  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TEXT, TXI.LONG_TEXT
+  TXI.SEQ_NBR, TXI.LANG_CODE, TXI.TAB_ALIAS, TXI.UPDATE_TIMESTAMP, TXI.UPDATE_USERID, TXI.TEXT, TXI.LONG_TEXT
 from TEXT_HEADERS TXT
 join TEXT_TYPE_HEADERS TTH
   on TTH.TXT_ID = TXT.ID
