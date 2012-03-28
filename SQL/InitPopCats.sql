340f6d3c4e82f586d4d5c3b6aa182f47166d587a
Peter Scott <scott@unhcr.org>
2012-03-28 11:40:57 +0000

    Textual updates.

--- SQL/InitPopCats.sql
+++ SQL/InitPopCats.sql
@@ -6,8 +6,8 @@ execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('IDP', 'en', 'Internall
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('IOC', 'en', '(?) Internally displaced others of concern');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('OOC', 'en', 'Others of concern');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RAS', 'en', '(?) Returnee asylum seekers');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RDP', 'en', '(?) Returnee internally displaced persons');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RDP', 'en', '(?) Returned internally displaced persons');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('REF', 'en', 'Refugees');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RET', 'en', 'Returnee refugees');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('ROC', 'en', '(?) Returnee others of concern');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('STA', 'en', 'Stateless');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('STA', 'en', 'Stateless persons');
