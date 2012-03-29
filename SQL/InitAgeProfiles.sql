d9ba2a0bf75703287f8e97b46f5e47ff79049392
Peter Scott <scott@unhcr.org>
2012-03-29 08:54:35 +0000

    Initial import.

---
+++ SQL/InitAgeProfiles.sql
@@ -0,0 +1,9 @@
+variable VERSION_NBR number;
+variable TXI_SEQ_NBR number;
+
+execute P_AGE_PROFILE.INSERT_AGE_PROFILE('STD', 'en', 'UNHCR standard age profile');
+execute P_AGE_PROFILE.INSERT_AGE_RANGE('STD', 0, 4, 'en', '0-4 years');
+execute P_AGE_PROFILE.INSERT_AGE_RANGE('STD', 5, 11, 'en', '5-11 years');
+execute P_AGE_PROFILE.INSERT_AGE_RANGE('STD', 12, 17, 'en', '12-17 years');
+execute P_AGE_PROFILE.INSERT_AGE_RANGE('STD', 18, 59, 'en', '18-59 years');
+execute P_AGE_PROFILE.INSERT_AGE_RANGE('STD', 60, 999, 'en', '60 years or over');
