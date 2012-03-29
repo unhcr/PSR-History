e49be3901ed8b1765db099c0665fc7c6ca520e7a
Peter Scott <scott@unhcr.org>
2012-03-29 15:16:23 +0000

    Add P_TIME_PERIOD package.

--- PLSQL/Packages.sql
+++ PLSQL/Packages.sql
@@ -11,6 +11,7 @@
 @@P_SYSTEM_USER.pkh
 @@P_TEXT.pkh
 @@P_TEXT_TYPE.pkh
+@@P_TIME_PERIOD.pkh
 
 @@PLS_UTILITY.pkb
 @@P_BASE.pkb
@@ -25,3 +26,4 @@
 @@P_SYSTEM_USER.pkb
 @@P_TEXT.pkb
 @@P_TEXT_TYPE.pkb
+@@P_TIME_PERIOD.pkb
