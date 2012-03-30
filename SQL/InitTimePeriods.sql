e47d72e8d478cd5696efc156df87b731f69ad385
Peter Scott <scott@unhcr.org>
2012-03-30 07:16:54 +0000

    More elegant procedure for generating time periods.

--- SQL/InitTimePeriods.sql
+++ SQL/InitTimePeriods.sql
@@ -6,24 +6,22 @@ execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('DAY', 'en', 'Day');
 declare
   dDay date := date '1950-01-01';
 begin
-  for nYear in 1950..2020
-  loop
-    P_TIME_PERIOD.INSERT_TIME_PERIOD(add_months(date '0001-01-01', 12 * (nYear-1)), add_months(date '0001-01-01', 12 * nYear), 'YEAR');
-  --
-    for nQtr in 1..4
-    loop
-      P_TIME_PERIOD.INSERT_TIME_PERIOD(add_months(date '0001-01-01', (12 * (nYear-1)) + (3 * (nQtr-1))), add_months(date '0001-01-01', (12 * (nYear-1)) + (3 * nQtr)), 'QTR');
-    end loop;
-  --
-    for nMonth in 1..12
-    loop
-      P_TIME_PERIOD.INSERT_TIME_PERIOD(add_months(date '0001-01-01', (12 * (nYear-1)) + (nMonth-1)), add_months(date '0001-01-01', (12 * (nYear-1)) + nMonth), 'MONTH');
-    end loop;
-  end loop;
---
   while dDay < date '2020-01-01'
   loop
+    if dDay = trunc(dDay, 'YYYY')
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 12), 'YEAR');
+    end if;
+  --
+    if dDay = trunc(dDay, 'Q')
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 3), 'QTR');
+    end if;
+  --
+    if dDay = trunc(dDay, 'MM')
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 1), 'MONTH');
+    end if;
+  --
     P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, dDay + 1, 'DAY');
+  --
     dDay := dDay + 1;
   end loop;
 end;
