322f8387c12b94daa280d30ea3b9096cfa335f37
Peter Scott <scott@unhcr.org>
2012-04-03 13:01:56 +0000

    Remove FALLBACK LANGUAGE system parameter (function replaced by LANGUAGES.DISPLAY_SEQ).

--- SQL/InitParameters.sql
+++ SQL/InitParameters.sql
@@ -1,7 +1,6 @@
 variable VERSION_NBR number;
 variable TXI_SEQ_NBR number;
 
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('FALLBACK LANGUAGE', 'en', 'System default language in the absence of any user preference', 'C', psCHAR_VALUE => 'en');
 execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MINIMUM DATE', 'en', 'Minimum date value (earlier than any possible practical date)', 'D', pdDATE_VALUE => date '0001-01-01');
 execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MAXIMUM DATE', 'en', 'Maximum date value (later than any possible practical date)', 'D', pdDATE_VALUE => timestamp '9999-12-31 23:59:59');
 execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('FALSE DATE', 'en', 'Impossible date value (used for parameter default values)', 'D', pdDATE_VALUE => date '1582-10-10');
