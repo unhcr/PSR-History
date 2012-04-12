1c7c200876a6021792bc2f42a5950bb53b5e14fc
Peter Scott <scott@unhcr.org>
2012-04-12 15:28:15 +0000

    Correct population category descriptions.

--- SQL/InitPopCats.sql
+++ SQL/InitPopCats.sql
@@ -3,11 +3,11 @@ variable TXI_SEQ_NBR number;
 
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('ASY', 'en', 'Asylum seekers');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('IDP', 'en', 'Internally displaced persons');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('IOC', 'en', '(?) Internally displaced others of concern');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('IOC', 'en', 'People in IDP-like situations');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('OOC', 'en', 'Others of concern');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RAS', 'en', '(?) Returnee asylum seekers');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RDP', 'en', '(?) Returned internally displaced persons');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RAS', 'en', 'Refugees and asylum seekers');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RDP', 'en', 'Returned internally displaced persons');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('REF', 'en', 'Refugees');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('RET', 'en', 'Returnee refugees');
-execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('ROC', 'en', '(?) Returnee others of concern');
+execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('ROC', 'en', 'People in refugee-like situations');
 execute P_POPULATION_CATEGORY.INSERT_POPULATION_CATEGORY('STA', 'en', 'Stateless persons');
