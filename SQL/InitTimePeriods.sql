6e2512ec69b97996d492a43db2431b23892bc30b
Peter Scott <scott@unhcr.org>
2012-04-12 15:31:02 +0000

    Changes for synthetic identifier-based time periods.

--- SQL/InitTimePeriods.sql
+++ SQL/InitTimePeriods.sql
@@ -4,23 +4,24 @@ execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('MONTH', 'en', 'Month');
 execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('DAY', 'en', 'Day');
 
 declare
+  nID P_BASE.tnPER_ID;
   dDay date := date '1950-01-01';
 begin
   while dDay < date '2020-01-01'
   loop
     if dDay = trunc(dDay, 'YYYY')
-    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 12), 'YEAR');
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(nID, 'YEAR', dDay, add_months(dDay, 12));
     end if;
   --
     if dDay = trunc(dDay, 'Q')
-    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 3), 'QTR');
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(nID, 'QTR', dDay, add_months(dDay, 3));
     end if;
   --
     if dDay = trunc(dDay, 'MM')
-    then P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, add_months(dDay, 1), 'MONTH');
+    then P_TIME_PERIOD.INSERT_TIME_PERIOD(nID, 'MONTH', dDay, add_months(dDay, 1));
     end if;
   --
-    P_TIME_PERIOD.INSERT_TIME_PERIOD(dDay, dDay + 1, 'DAY');
+    P_TIME_PERIOD.INSERT_TIME_PERIOD(nID, 'DAY', dDay, dDay + 1);
   --
     dDay := dDay + 1;
   end loop;
