c3bd138389e4d4e5f8921901790d703a3488a1c8
Peter Scott <scott@unhcr.org>
2012-04-13 14:15:49 +0000

    Fix error in non-ascii characters in sex descriptions.

--- SQL/InitSexes.sql
+++ SQL/InitSexes.sql
@@ -5,14 +5,14 @@ execute :TXT_ID := null; :TXI_SEQ_NBR := null; P_TEXT.SET_TEXT(:TXT_ID, 'SEX', '
 insert into SEXES (CODE, DISPLAY_SEQ, TXT_ID) values ('F', 1, :TXT_ID);
 execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'fr', 'femelle');
 execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'es', 'femenino');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ru', '\0436\0435\043D\0449\0438\043D\0430');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ar', '\0623\0646\062B\0649');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'zh', '\5973');
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ru', unistr('\0436\0435\043D\0449\0438\043D\0430'));
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ar', unistr('\0623\0646\062B\0649'));
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'zh', unistr('\5973'));
 
 execute :TXT_ID := null; :TXI_SEQ_NBR := null; P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'en', 'Male');
 insert into SEXES (CODE, DISPLAY_SEQ, TXT_ID) values ('M', 2, :TXT_ID);
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'fr', 'm\00E2le');
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'fr', unistr('m\00E2le'));
 execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'es', 'masculino');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ru', '\043C\0443\0436\0447\0438\043D\0430');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ar', '\0630\0643\0631');
-execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'zh', '\7537');
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ru', unistr('\043C\0443\0436\0447\0438\043D\0430'));
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'ar', unistr('\0630\0643\0631'));
+execute P_TEXT.SET_TEXT(:TXT_ID, 'SEX', 'DESCR', :TXI_SEQ_NBR, 'zh', unistr('\7537'));
