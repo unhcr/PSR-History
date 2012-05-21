0078ade05eb130c3a4c2f5dd544100aacb6f78d6
Peter Scott <scott@unhcr.org>
2012-05-21 08:56:53 +0000

    Rationalise naming of called scripts. Improve script layout.

--- SQL/Rebuild.sql
+++ SQL/Rebuild.sql
@@ -8,51 +8,78 @@ purge recyclebin;
 prompt PSRD
 prompt ====
 @PSRD
+
 prompt Packages
 prompt ========
 @Packages
+
 prompt Triggers
 prompt ========
 @Triggers
+
 prompt Views
 prompt =====
 @Views
+
 prompt InitBase
 prompt ========
 @InitBase
+
 prompt InitMessages
 prompt ============
 @InitMessages
+
 prompt InitParameters
 prompt ==============
 @InitParameters
-prompt InitLanguages
+
+prompt LoadLanguages
 prompt =============
-@InitLanguages
+@LoadLanguages
+
 prompt InitStatisticTypes
 prompt =============
 @InitStatisticTypes
+
 prompt InitDisplacementStatuses
 prompt ===========
 @InitDisplacementStatuses
+
 prompt InitSexes
 prompt =========
 @InitSexes
+
 prompt InitAgeProfiles
 prompt ===============
 @InitAgeProfiles
+
 prompt InitTimePeriods
 prompt ===============
 @InitTimePeriods
+
 prompt InitDimensions
 prompt ==============
 @InitDimensions
+
 prompt InitLocations
 prompt =============
 @InitLocations
-prompt InitStatistics
+
+prompt LoadLocations_Countries
+prompt =======================
+@LoadLocations_Countries
+
+prompt LoadLocations_Subdivisions
+prompt ==========================
+@LoadLocations_Subdivisions
+
+prompt LoadPPGs
+prompt ========
+@LoadPPGs
+
+prompt LoadStatistics
 prompt ==============
-@InitStatistics
+@LoadStatistics
 
 commit;
 
