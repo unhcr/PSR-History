d5d3e9993dc3523f5f1c1daf2c1696a442bb710b
Peter Scott <scott@unhcr.org>
2012-08-10 06:43:48 +0000

    Add new code RPT (repatriated refugees) and modify some descriptions.

--- SQL/InitDisplacementStatuses.sql
+++ SQL/InitDisplacementStatuses.sql
@@ -2,12 +2,13 @@ variable VERSION_NBR number;
 variable TXT_SEQ_NBR number;
 
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('REF', 'en', 'Refugees', 1);
-execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('ROC', 'en', 'People in refugee-like situations', 2);
+execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('ROC', 'en', 'Persons in a refugee-like situation', 2);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('ASY', 'en', 'Asylum seekers', 3);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('RET', 'en', 'Returned refugees', 4);
+execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('RPT', 'en', 'Repatriated refugees', 4);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('IDP', 'en', 'Internally displaced persons', 5);
-execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('IOC', 'en', 'People in IDP-like situations', 6);
-execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('RDP', 'en', 'Returned internally displaced persons', 7);
+execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('IOC', 'en', 'Persons in an IDP-like situation', 6);
+execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('RDP', 'en', 'Persons returned from an IDP or IDP-like situation', 7);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('STA', 'en', 'Stateless persons', 8);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('OOC', 'en', 'Others of concern', 9);
 execute P_DISPLACEMENT_STATUS.INSERT_DISPLACEMENT_STATUS('RAS', 'en', 'Refugees and asylum seekers');
