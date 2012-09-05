f6bfcc7c4f63ce145c2f26bf60b96363d4e7f063
Peter Scott <scott@unhcr.org>
2012-09-05 06:27:37 +0000

    Temporary version which inserts directly into base tables for speed, instead of using the API procedures.

--- SQL/InitTimePeriods.sql
+++ SQL/InitTimePeriods.sql
@@ -3,6 +3,7 @@ execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('QTR', 'en', 'Quarter');
 execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('MONTH', 'en', 'Month');
 execute P_TIME_PERIOD.INSERT_TIME_PERIOD_TYPE('DAY', 'en', 'Day');
 
+/*
 declare
   nID P_BASE.tnPER_ID;
   dDay date := date '2000-01-01';
@@ -22,6 +23,37 @@ begin
     end if;
   --
     P_TIME_PERIOD.INSERT_TIME_PERIOD(nID, 'DAY', dDay, dDay + 1);
+  --
+    dDay := dDay + 1;
+  end loop;
+end;
+*/
+
+declare
+  dDay date := date '2000-01-01';
+begin
+  while dDay < date '2020-01-01'
+  loop
+    if dDay = trunc(dDay, 'YYYY')
+    then
+      insert into T_TIME_PERIODS (ID, PERT_CODE, START_DATE, END_DATE)
+      values (PER_SEQ.nextval, 'YEAR', dDay, add_months(dDay, 12));
+    end if;
+  --
+    if dDay = trunc(dDay, 'Q')
+    then
+      insert into T_TIME_PERIODS (ID, PERT_CODE, START_DATE, END_DATE)
+      values (PER_SEQ.nextval, 'QTR', dDay, add_months(dDay, 3));
+    end if;
+  --
+    if dDay = trunc(dDay, 'MM')
+    then
+      insert into T_TIME_PERIODS (ID, PERT_CODE, START_DATE, END_DATE)
+      values (PER_SEQ.nextval, 'MONTH', dDay, add_months(dDay, 1));
+    end if;
+  --
+    insert into T_TIME_PERIODS (ID, PERT_CODE, START_DATE, END_DATE)
+    values (PER_SEQ.nextval, 'DAY', dDay, dDay + 1);
   --
     dDay := dDay + 1;
   end loop;
