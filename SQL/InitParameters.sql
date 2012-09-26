a1e2ad6b8db1d5fd27dc4da6c26f4d3ba4e51713
Peter Scott <scott@unhcr.org>
2012-09-26 09:30:51 +0000

    Rewrite as PL/SQL block.

--- SQL/InitParameters.sql
+++ SQL/InitParameters.sql
@@ -1,9 +1,12 @@
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MINIMUM DATE', 'en', 'Minimum date value (earlier than any possible practical date)', 'D', pdDATE_VALUE => date '0001-01-01');
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MAXIMUM DATE', 'en', 'Maximum date value (later than any possible practical date)', 'D', pdDATE_VALUE => timestamp '9999-12-31 23:59:59');
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('FALSE DATE', 'en', 'Impossible date value (used for parameter default values)', 'D', pdDATE_VALUE => date '1582-10-10');
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID MULTIPLIER', 'en', 'Multiplier used in calculating hashed location identifier', 'N', pnNUM_VALUE => 242701457);
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID INCREMENT', 'en', 'Increment used in calculating hashed location identifier', 'N', pnNUM_VALUE => 31415927);
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK MULTIPLIER', 'en', 'Multiplier used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 43);
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK INCREMENT', 'en', 'Increment used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 99);
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK MODULUS', 'en', 'Modulus used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 89);
-execute P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('COUNTRY LOCAT CODE', 'en', 'Location type code for standard country code', 'C', psCHAR_VALUE => 'IS03166A3');
+begin
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MINIMUM DATE', 'en', 'Minimum date value (earlier than any possible practical date)', 'D', pdDATE_VALUE => date '0001-01-01');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('MAXIMUM DATE', 'en', 'Maximum date value (later than any possible practical date)', 'D', pdDATE_VALUE => timestamp '9999-12-31 23:59:59');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('FALSE DATE', 'en', 'Impossible date value (used for parameter default values)', 'D', pdDATE_VALUE => date '1582-10-10');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID MULTIPLIER', 'en', 'Multiplier used in calculating hashed location identifier', 'N', pnNUM_VALUE => 242701457);
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID INCREMENT', 'en', 'Increment used in calculating hashed location identifier', 'N', pnNUM_VALUE => 31415927);
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK MULTIPLIER', 'en', 'Multiplier used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 43);
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK INCREMENT', 'en', 'Increment used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 99);
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK MODULUS', 'en', 'Modulus used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 89);
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('COUNTRY LOCAT CODE', 'en', 'Location type code for standard country code', 'C', psCHAR_VALUE => 'IS03166A3');
+end;
+/
--- SQL/InitSexes.sql
+++ SQL/InitSexes.sql
@@ -1,18 +1,25 @@
-variable ITM_ID number;
-variable TXT_SEQ_NBR number;
-
-execute :ITM_ID := null; :TXT_SEQ_NBR := null; P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'en', 'Female');
-insert into T_SEXES (CODE, DISPLAY_SEQ, ITM_ID) values ('F', 1, :ITM_ID);
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'fr', 'femelle');
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'es', 'femenino');
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'ru', unistr('\0436\0435\043D\0449\0438\043D\0430'));
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'ar', unistr('\0623\0646\062B\0649'));
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'zh', unistr('\5973'));
-
-execute :ITM_ID := null; :TXT_SEQ_NBR := null; P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'en', 'Male');
-insert into T_SEXES (CODE, DISPLAY_SEQ, ITM_ID) values ('M', 2, :ITM_ID);
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'fr', unistr('m\00E2le'));
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'es', 'masculino');
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'ru', unistr('\043C\0443\0436\0447\0438\043D\0430'));
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'ar', unistr('\0630\0643\0631'));
-execute P_TEXT.SET_TEXT(:ITM_ID, 'SEX', 'DESCR', :TXT_SEQ_NBR, 'zh', unistr('\7537'));
+declare
+  nITM_ID number;
+  nTXT_SEQ_NBR number;
+begin
+  nITM_ID := null;
+  nTXT_SEQ_NBR := null;
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'en', 'Female');
+  insert into T_SEXES (CODE, DISPLAY_SEQ, ITM_ID) values ('F', 1, nITM_ID);
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'fr', 'femelle');
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'es', 'femenino');
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'ru', unistr('\0436\0435\043D\0449\0438\043D\0430'));
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'ar', unistr('\0623\0646\062B\0649'));
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'zh', unistr('\5973'));
+--
+  nITM_ID := null;
+  nTXT_SEQ_NBR := null;
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'en', 'Male');
+  insert into T_SEXES (CODE, DISPLAY_SEQ, ITM_ID) values ('M', 2, nITM_ID);
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'fr', unistr('m\00E2le'));
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'es', 'masculino');
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'ru', unistr('\043C\0443\0436\0447\0438\043D\0430'));
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'ar', unistr('\0630\0643\0631'));
+  P_TEXT.SET_TEXT(nITM_ID, 'SEX', 'DESCR', nTXT_SEQ_NBR, 'zh', unistr('\7537'));
+end;
+/
