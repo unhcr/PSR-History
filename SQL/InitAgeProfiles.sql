5ef37c2b0e648e0da84487480f2b6784a58f8fc0
Peter Scott <scott@unhcr.org>
2012-09-26 09:29:21 +0000

    Rewrite as PL/SQL block.

--- SQL/InitAgeProfiles.sql
+++ SQL/InitAgeProfiles.sql
@@ -1,9 +1,12 @@
-variable AGR_ID number;
-
-execute P_AGE_PROFILE.INSERT_AGE_PROFILE('STD', 'en', 'UNHCR standard age profile');
-
-execute P_AGE_PROFILE.INSERT_AGE_RANGE(:AGR_ID, 'en', '0-4 years', 'STD', 0, 4);
-execute P_AGE_PROFILE.INSERT_AGE_RANGE(:AGR_ID, 'en', '5-11 years', 'STD', 5, 11);
-execute P_AGE_PROFILE.INSERT_AGE_RANGE(:AGR_ID, 'en', '12-17 years', 'STD', 12, 17);
-execute P_AGE_PROFILE.INSERT_AGE_RANGE(:AGR_ID, 'en', '18-59 years', 'STD', 18, 59);
-execute P_AGE_PROFILE.INSERT_AGE_RANGE(:AGR_ID, 'en', '60 years or over', 'STD', 60, 999);
+declare
+  nAGR_ID P_BASE.tnAGR_ID;
+begin
+  P_AGE_PROFILE.INSERT_AGE_PROFILE('STD', 'en', 'UNHCR standard age profile');
+--
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '0-4 years', 'STD', 0, 4);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '5-11 years', 'STD', 5, 11);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '12-17 years', 'STD', 12, 17);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '18-59 years', 'STD', 18, 59);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '60 years or over', 'STD', 60, 999);
+end;
+/
