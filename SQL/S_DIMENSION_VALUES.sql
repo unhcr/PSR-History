fd1a99eb6679cfece17ab082bed57e8a05b6d07a
Peter Scott <scott@unhcr.org>
2012-09-26 09:21:51 +0000

    Add ACTIVE_FLAG.

--- SQL/S_DIMENSION_VALUES.sql
+++ SQL/S_DIMENSION_VALUES.sql
@@ -3,6 +3,7 @@ create table S_DIMENSION_VALUES
   CODE varchar2(10),
   DESCRIPTION_EN varchar2(1000),
   DISPLAY_SEQ number(5),
+  ACTIVE_FLAG varchar2(1),
   DESCRIPTION_FR varchar2(1000),
   NOTES varchar2(4000))
 organization external
@@ -23,6 +24,7 @@ organization external
       CODE char(4000),
       DESCRIPTION_EN char(4000),
       DISPLAY_SEQ char(4000),
+      ACTIVE_FLAG char(4000),
       DESCRIPTION_FR char(4000),
       NOTES char(4000)))
   location ('DIMENSION_VALUES.csv'))
