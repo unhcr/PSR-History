c20029fe867ce7d01fa91c9f7e46980ab990d0c0
Peter Scott <scott@unhcr.org>
2012-09-26 09:53:05 +0000

    Load language names in all UN languages. Load notes. Cater for updating pre-existing lanugages.

--- SQL/LoadLanguages.sql
+++ SQL/LoadLanguages.sql
@@ -1,16 +1,56 @@
+set serveroutput on
+
 declare
   nVERSION_NBR P_BASE.tnLANG_VERSION_NBR;
+  nSEQ_NBR P_BASE.tnTXT_SEQ_NBR;
+  iCount pls_integer := 0;
 begin
   for rLANG in
-   (select CODE, NAME_EN, DISPLAY_SEQ, ACTIVE_FLAG, NAME_FR
-    from STAGE.S_LANGUAGES
-    where CODE not in
-     (select CODE from T_LANGUAGES)
-    order by CODE)
+   (select A.CODE, A.NAME_EN, A.DISPLAY_SEQ, A.ACTIVE_FLAG,
+      A.NAME_FR, A.NAME_ES, A.NAME_RU, A.NAME_AR, A.NAME_ZH,
+      A.NOTES, B.VERSION_NBR
+    from STAGE.S_LANGUAGES A
+    left outer join T_LANGUAGES B
+      on B.CODE = A.CODE
+    order by A.CODE)
   loop
-    P_LANGUAGE.INSERT_LANGUAGE(rLANG.CODE, 'en', rLANG.NAME_EN, rLANG.DISPLAY_SEQ, rLANG.ACTIVE_FLAG);
-    nVERSION_NBR := 1;
-    P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'fr', rLANG.NAME_FR);
+    iCount := iCount + 1;
+  --
+    nVERSION_NBR := rLANG.VERSION_NBR;
+    P_LANGUAGE.SET_LANGUAGE(rLANG.CODE, nVERSION_NBR, 'en', rLANG.NAME_EN, rLANG.DISPLAY_SEQ, rLANG.ACTIVE_FLAG);
+  --
+    if rLANG.NAME_FR is not null
+    then
+      P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'fr', rLANG.NAME_FR);
+    end if;
+  --
+    if rLANG.NAME_ES is not null
+    then
+      P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'es', rLANG.NAME_ES);
+    end if;
+  --
+    if rLANG.NAME_RU is not null
+    then
+      P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'ru', rLANG.NAME_RU);
+    end if;
+  --
+    if rLANG.NAME_AR is not null
+    then
+      P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'ar', rLANG.NAME_AR);
+    end if;
+  --
+    if rLANG.NAME_ZH is not null
+    then
+      P_LANGUAGE.SET_LANG_DESCRIPTION(rLANG.CODE, nVERSION_NBR, 'zh', rLANG.NAME_ZH);
+    end if;
+  --
+    if rLANG.NOTES is not null
+    then
+      nSEQ_NBR := null;
+      P_LANGUAGE.SET_LANG_TEXT(rLANG.CODE, nVERSION_NBR, 'NOTE', nSEQ_NBR, 'en', rLANG.NOTES);
+    end if;
   end loop;
+--
+  dbms_output.put_line(to_char(iCount) || ' LANGUAGES records inserted');
 end;
 /
