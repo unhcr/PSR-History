bd91c630ba0cf2c33e4cf9120c1e2746e68efdb5
Peter Scott <scott@unhcr.org>
2012-11-06 11:42:13 +0000

    Initial import.

---
+++ SQL/S_DISPLACEMENT_STATUSES.sql
@@ -0,0 +1,33 @@
+create table S_DISPLACEMENT_STATUSES
+ (CODE varchar2(10),
+  DESCRIPTION_EN varchar2(1000),
+  START_DATE date,
+  END_DATE date,
+  DISPLAY_SEQ number(5),
+  DESCRIPTION_FR varchar2(1000),
+  NOTES varchar2(4000))
+organization external
+ (type oracle_loader
+  default directory PSRDATA
+  access parameters 
+   (records delimited by newline
+    characterset WE8MSWIN1252
+    badfile 'DISPLACEMENT_STATUSES.bad'
+    nodiscardfile
+    nologfile
+    skip 1 
+    fields terminated by ','
+    optionally enclosed by '"' and '"'
+    lrtrim
+    missing field values are null
+     (CODE char(4000),
+      DESCRIPTION_EN char(4000),
+      START_DATE date "YYYY-MM-DD",
+      END_DATE date "YYYY-MM-DD",
+      DISPLAY_SEQ char(4000),
+      DESCRIPTION_FR char(4000),
+      NOTES char(4000)))
+  location ('DISPLACEMENT_STATUSES.csv'))
+reject limit unlimited;
+  
+grant select on S_DISPLACEMENT_STATUSES to PSR_STAGE;
