b8bc5ab2d31a84305da7b96e5abf627f6ab665c3
Peter Scott <scott@unhcr.org>
2012-11-16 16:02:59 +0000

    Initialise additional system parameters concerned with tracing and logging.

--- SQL/InitParameters.sql
+++ SQL/InitParameters.sql
@@ -8,5 +8,11 @@ begin
   P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK INCREMENT', 'en', 'Increment used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 99);
   P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOCATION ID CHECK MODULUS', 'en', 'Modulus used in calculating check digits for location identifier', 'N', pnNUM_VALUE => 89);
   P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('COUNTRY LOCAT CODE', 'en', 'Location type code for standard country code', 'C', psCHAR_VALUE => 'IS03166A3');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('DISPLAY CALL STACK', 'en', 'Display the call stack in exception messages? (Y/N)', 'C', psCHAR_VALUE => 'Y');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('DISPLAY BACKTRACE', 'en', 'Display backtrace information in exception messages? (Y/N)', 'C', psCHAR_VALUE => 'Y');
+  --P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('TRACING', 'en', 'Display continuous trace information? (Y/N)', 'C', psCHAR_VALUE => 'Y');
+  --P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('TRACE TO LOG', 'en', 'Write continuous trace information to log file? (Y/N)', 'C', psCHAR_VALUE => 'Y');
+  P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('LOG DIRECTORY', 'en', 'Name of Oracle directory to which log files will be written', 'C', psCHAR_VALUE => 'ORATRACE');
+  --P_SYSTEM_PARAMETER.INSERT_SYSTEM_PARAMETER('TRACE LOG FILE NAME', 'en', 'Name of continuous trace log file', 'C', psCHAR_VALUE => 'TraceLog.log');
 end;
 /
