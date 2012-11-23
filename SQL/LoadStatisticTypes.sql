45fade34eaae4441a0ab105d13b3c2aa380cede5
Peter Scott <scott@unhcr.org>
2012-11-23 16:22:46 +0000

    Changes related to renaming of T_STATISTIC_GROUP to T_STATISTIC_TYPE_GROUP.

--- SQL/LoadStatisticTypes.sql
+++ SQL/LoadStatisticTypes.sql
@@ -1,35 +1,35 @@
 set serveroutput on
 
 declare
-  nSTCG_VERSION_NBR P_BASE.tnSTCG_VERSION_NBR;
+  nSTTG_VERSION_NBR P_BASE.tnSTTG_VERSION_NBR;
   nSTCT_VERSION_NBR P_BASE.tnSTCT_VERSION_NBR;
-  nSTCTG_VERSION_NBR P_BASE.tnSTCTG_VERSION_NBR;
+  nSTTIG_VERSION_NBR P_BASE.tnSTTIG_VERSION_NBR;
   nSEQ_NBR P_BASE.tnTXT_SEQ_NBR;
   iCount1 pls_integer := 0;
   iCount2 pls_integer := 0;
   iCount3 pls_integer := 0;
 begin
-  for rSTCG in
+  for rSTTG in
    (select CODE, DESCRIPTION_EN, DISPLAY_SEQ, DESCRIPTION_FR, NOTES
-    from STAGE.S_STATISTIC_GROUPS)
+    from STAGE.S_STATISTIC_TYPE_GROUPS)
   loop
     iCount1 := iCount1 + 1;
   --
-    P_STATISTIC_TYPE.INSERT_STATISTIC_GROUP
-     (rSTCG.CODE, 'en', rSTCG.DESCRIPTION_EN, rSTCG.DISPLAY_SEQ);
-    nSTCG_VERSION_NBR := 1;
+    P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUP
+     (rSTTG.CODE, 'en', rSTTG.DESCRIPTION_EN, rSTTG.DISPLAY_SEQ);
+    nSTTG_VERSION_NBR := 1;
   --
-    if rSTCG.DESCRIPTION_FR is not null
+    if rSTTG.DESCRIPTION_FR is not null
     then
-      P_STATISTIC_TYPE.SET_STCG_DESCRIPTION
-       (rSTCG.CODE, nSTCG_VERSION_NBR, 'fr', rSTCG.DESCRIPTION_FR);
+      P_STATISTIC_TYPE.SET_STTG_DESCRIPTION
+       (rSTTG.CODE, nSTTG_VERSION_NBR, 'fr', rSTTG.DESCRIPTION_FR);
     end if;
   --
-    if rSTCG.NOTES is not null
+    if rSTTG.NOTES is not null
     then
       nSEQ_NBR := null;
-      P_STATISTIC_TYPE.SET_STCG_TEXT
-       (rSTCG.CODE, nSTCG_VERSION_NBR, 'NOTE', nSEQ_NBR, 'en', rSTCG.NOTES);
+      P_STATISTIC_TYPE.SET_STTG_TEXT
+       (rSTTG.CODE, nSTTG_VERSION_NBR, 'NOTE', nSEQ_NBR, 'en', rSTTG.NOTES);
     end if;
   end loop;
 --
@@ -39,7 +39,7 @@ begin
       LOC_ID_ORIGIN_COUNTRY_FLAG, LOC_ID_ORIGIN_FLAG,
       DIM_ID1_FLAG, DIMT_CODE1, DIM_ID2_FLAG, DIMT_CODE2, DIM_ID3_FLAG, DIMT_CODE3,
       DIM_ID4_FLAG, DIMT_CODE4, DIM_ID5_FLAG, DIMT_CODE5, SEX_CODE_FLAG, AGR_ID_FLAG,
-      PGR_ID_SUBGROUP_FLAG, STCG_CODE1, STCG_CODE2, STCG_CODE3, STCG_CODE4,
+      PGR_ID_SUBGROUP_FLAG, STTG_CODE1, STTG_CODE2, STTG_CODE3, STTG_CODE4,
       DISPLAY_SEQ, DESCRIPTION_FR, NOTES
     from STAGE.S_STATISTIC_TYPES)
   loop
@@ -55,28 +55,28 @@ begin
       rSTCT.PGR_ID_SUBGROUP_FLAG, rSTCT.DISPLAY_SEQ);
     nSTCT_VERSION_NBR := 1;
   --
-    if rSTCT.STCG_CODE1 is not null
+    if rSTCT.STTG_CODE1 is not null
     then
       iCount3 := iCount3 + 1;
-      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUPING(rSTCT.STCG_CODE1, rSTCT.CODE);
+      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_IN_GROUP(rSTCT.STTG_CODE1, rSTCT.CODE);
     end if;
   --
-    if rSTCT.STCG_CODE2 is not null
+    if rSTCT.STTG_CODE2 is not null
     then
       iCount3 := iCount3 + 1;
-      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUPING(rSTCT.STCG_CODE2, rSTCT.CODE);
+      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_IN_GROUP(rSTCT.STTG_CODE2, rSTCT.CODE);
     end if;
   --
-    if rSTCT.STCG_CODE3 is not null
+    if rSTCT.STTG_CODE3 is not null
     then
       iCount3 := iCount3 + 1;
-      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUPING(rSTCT.STCG_CODE3, rSTCT.CODE);
+      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_IN_GROUP(rSTCT.STTG_CODE3, rSTCT.CODE);
     end if;
   --
-    if rSTCT.STCG_CODE4 is not null
+    if rSTCT.STTG_CODE4 is not null
     then
       iCount3 := iCount3 + 1;
-      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUPING(rSTCT.STCG_CODE4, rSTCT.CODE);
+      P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_IN_GROUP(rSTCT.STTG_CODE4, rSTCT.CODE);
     end if;
   --
     if rSTCT.DESCRIPTION_FR is not null
@@ -93,27 +93,27 @@ begin
     end if;
   end loop;
 --
-  for rSTCTG in
-   (select STCG_CODE, STCT_CODE, NOTES
+  for rSTTIG in
+   (select STTG_CODE, STCT_CODE, NOTES
     from STAGE.S_STATISTIC_TYPES_IN_GROUPS
-    where (STCG_CODE, STCT_CODE) not in
-     (select STCG_CODE, STCT_CODE
+    where (STTG_CODE, STCT_CODE) not in
+     (select STTG_CODE, STCT_CODE
       from T_STATISTIC_TYPES_IN_GROUPS))
   loop
     iCount3 := iCount3 + 1;
   --
-    P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_GROUPING(rSTCTG.STCG_CODE, rSTCTG.STCT_CODE);
-    nSTCTG_VERSION_NBR := 1;
+    P_STATISTIC_TYPE.INSERT_STATISTIC_TYPE_IN_GROUP(rSTTIG.STTG_CODE, rSTTIG.STCT_CODE);
+    nSTTIG_VERSION_NBR := 1;
   --
-    if rSTCTG.NOTES is not null
+    if rSTTIG.NOTES is not null
     then
       nSEQ_NBR := null;
-      P_STATISTIC_TYPE.SET_STCTG_TEXT
-       (rSTCTG.STCG_CODE, rSTCTG.STCT_CODE, nSTCTG_VERSION_NBR, 'NOTE', nSEQ_NBR, 'en', rSTCTG.NOTES);
+      P_STATISTIC_TYPE.SET_STTIG_TEXT
+       (rSTTIG.STTG_CODE, rSTTIG.STCT_CODE, nSTTIG_VERSION_NBR, 'NOTE', nSEQ_NBR, 'en', rSTTIG.NOTES);
     end if;
   end loop;
 --
-  dbms_output.put_line(to_char(iCount1) || ' STATISTIC_GROUPS records inserted');
+  dbms_output.put_line(to_char(iCount1) || ' STATISTIC_TYPE_GROUPS records inserted');
   dbms_output.put_line(to_char(iCount2) || ' STATISTIC_TYPES records inserted');
   dbms_output.put_line(to_char(iCount3) || ' STATISTIC_TYPES_IN_GROUPS records inserted');
 end;
