37f41acd656f74370876c8e1c4baa2f0de6488c0
Peter Scott <scott@unhcr.org>
2013-01-25 07:16:31 +0000

    Correct location relationship effective dating. Merge duplicate locations in countries that have split, merged, or changed their details.

--- SQL/LoadLocationsSubdivisions.sql
+++ SQL/LoadLocationsSubdivisions.sql
@@ -60,10 +60,10 @@ begin
       max(LOCTV.ID) as LOCTV_ID,
       count(distinct LOCTV.ID) LOCTV_COUNT
     from Q_ASR_LOCATIONS ASL
-    join COUNTRIES COU
+    inner join COUNTRIES COU
       on COU.UNHCR_COUNTRY_CODE = ASL.COU_CODE
-      and COU.START_DATE <= add_months(trunc(to_date(ASL.STATSYEAR, 'YYYY'), 'YYYY'), 12) - 1
-      and COU.END_DATE > add_months(trunc(to_date(ASL.STATSYEAR, 'YYYY'), 'YYYY'), 12) - 1
+      and COU.START_DATE < add_months(to_date(ASL.STATSYEAR || '0101', 'YYYYMMDD'), 12)
+      and COU.END_DATE >= add_months(to_date(ASL.STATSYEAR || '0101', 'YYYYMMDD'), 12)
     left outer join LOCATION_TYPE_VARIANTS LOCTV
       on LOCTV.LOC_ID = COU.ID
       and LOCTV.LOCRT_CODE = 'WITHIN'
@@ -74,8 +74,8 @@ begin
         end
     left outer join Q_MSRP_LOCATIONS MSL
       on MSL.COU_CODE = ASL.COU_CODE
-      and MSL.START_DATE <= add_months(trunc(to_date(ASL.STATSYEAR, 'YYYY'), 'YYYY'), 12) - 1
-      and MSL.END_DATE > add_months(trunc(to_date(ASL.STATSYEAR, 'YYYY'), 'YYYY'), 12) - 1
+      and MSL.START_DATE < add_months(to_date(ASL.STATSYEAR || '0101', 'YYYYMMDD'), 12)
+      and MSL.END_DATE >= add_months(to_date(ASL.STATSYEAR || '0101', 'YYYYMMDD'), 12)
       and upper(MSL.LOCATION_NAME) = upper(ASL.LOC_NAME)
     group by ASL.COU_CODE,
       COU.ID,
@@ -122,7 +122,7 @@ begin
       ALOC.LOCATION_NAME, ALOC.LOCATION_TYPE_DESCRIPTION, ALOC.LOCATION_TYPE_CODE,
       COU.ID as COU_ID, LOCTV.ID as LOCTV_ID
     from STAGE.S_ASR_ADDITIONAL_LOCATIONS ALOC
-    join COUNTRIES COU
+    inner join COUNTRIES COU
       on COU.UNHCR_COUNTRY_CODE = ALOC.UNHCR_COUNTRY_CODE
     left outer join LOCATION_TYPE_VARIANTS LOCTV
       on LOCTV.LOC_ID = COU.ID
@@ -144,5 +144,38 @@ begin
   end loop;
 --
   dbms_output.put_line(to_char(iCount) || ' LOCATIONS records inserted');
+--
+-- Merge duplicate locations in countries that have split, merged, or changed their details
+--
+  for rLOCR in
+   (select LOCR1.LOC_ID_FROM as LOC_ID_FROM1, LOCR1.LOC_ID_TO as LOC_ID_TO1,
+      LOCR1.VERSION_NBR as LOCR_VERSION_NBR, LOC1.VERSION_NBR as LOC_VERSION_NBR1,
+      LOCR2.LOC_ID_FROM as LOC_ID_FROM2, LOCR2.LOC_ID_TO as LOC_ID_TO2,
+      LOCR2.START_DATE, LOCR2.END_DATE, LOC2.VERSION_NBR as LOC_VERSION_NBR2,
+      LOCR1.LOCRT_CODE, LOCR.LOCRT_CODE as LOCRT_CODE1, LOC1.NAME
+    from T_LOCATION_RELATIONSHIPS LOCR
+    inner join T_LOCATION_RELATIONSHIPS LOCR1
+      on LOCR1.LOC_ID_FROM = LOCR.LOC_ID_FROM
+      and LOCR1.LOCRT_CODE not in ('CSPLIT', 'CMERGE', 'CCHANGE')
+    inner join LOCATIONS LOC1
+      on LOC1.ID = LOCR1.LOC_ID_TO
+    inner join T_LOCATION_RELATIONSHIPS LOCR2
+      on LOCR2.LOC_ID_FROM = LOCR.LOC_ID_TO
+      and LOCR2.LOCRT_CODE not in ('CSPLIT', 'CMERGE', 'CCHANGE')
+    inner join LOCATIONS LOC2
+      on LOC2.ID = LOCR2.LOC_ID_TO
+    where LOCR.LOCRT_CODE in ('CSPLIT', 'CMERGE', 'CCHANGE')
+    and LOCR2.LOCRT_CODE = LOCR1.LOCRT_CODE
+    and LOC2.LOCT_CODE != 'ADMIN0'
+    and LOC2.NAME = LOC1.NAME)
+  loop
+    P_LOCATION.UPDATE_LOCATION
+     (rLOCR.LOC_ID_TO1, rLOCR.LOC_VERSION_NBR1, pdEND_DATE => rLOCR.END_DATE);
+  --
+    P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+     (rLOCR.LOC_ID_FROM2, rLOCR.LOC_ID_TO1, rLOCR.LOCRT_CODE, rLOCR.START_DATE, rLOCR.END_DATE);
+  --
+    P_LOCATION.DELETE_LOCATION(rLOCR.LOC_ID_TO2, rLOCR.LOC_VERSION_NBR2);
+  end loop;
 end;
 /
