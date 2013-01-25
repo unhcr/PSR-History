b723419ef0613aaba898294b1464e16b6833cf9a
Peter Scott <scott@unhcr.org>
2013-01-25 07:14:44 +0000

    Load effective dates on location relationships. Add WITHIN relationships for UNSD geographical regions.

--- SQL/LoadLocationsRegions.sql
+++ SQL/LoadLocationsRegions.sql
@@ -7,6 +7,33 @@ declare
   nLOC_VERSION_NBR P_BASE.tnLOC_VERSION_NBR;
   nTXT_SEQ_NBR P_BASE.tnTXT_SEQ_NBR;
   iCount pls_integer := 0;
+--
+  procedure INSERT_LOCATION_WITHIN
+   (pnLOC_ID_FROM in P_BASE.tmnLOC_ID,
+    pnLOC_ID_TO in P_BASE.tmnLOC_ID,
+    pdSTART_DATE in P_BASE.tdDate,
+    pdEND_DATE in P_BASE.tdDate)
+  is
+    sDummy varchar2(1);
+  begin
+    begin
+      select 'x'
+      into sDummy
+      from T_LOCATION_RELATIONSHIPS
+      where LOC_ID_FROM = pnLOC_ID_FROM
+      and LOC_ID_TO = pnLOC_ID_TO
+      and LOCRT_CODE = 'WITHIN'
+      and START_DATE < nvl(pdEND_DATE, P_BASE.gdMAX_DATE)
+      and END_DATE > nvl(pdSTART_DATE, P_BASE.gdMIN_DATE);
+    --
+      return;
+    exception
+      when NO_DATA_FOUND
+      then null;
+    end;
+  --
+    P_LOCATION.INSERT_LOCATION_WITHIN(pnLOC_ID_FROM, pnLOC_ID_TO, pdSTART_DATE, pdEND_DATE);
+  end INSERT_LOCATION_WITHIN;
 begin
   for rLOC in
    (select KEY, NAME_EN, LOCT_CODE, START_DATE, END_DATE, NAME_FR, UNSDNUM, HCRCD,
@@ -53,38 +80,50 @@ begin
   iCount := 0;
 --
   for rLOC in
-   (select KEY, UNSD_FROM_KEY, UNSDA_FROM_KEY, HCRRESP_FROM_KEY, RSGEO_FROM_KEY, RSHCR_FROM_KEY
+   (select KEY, START_DATE, END_DATE,
+      UNSD_FROM_KEY, UNSDA_FROM_KEY, HCRRESP_FROM_KEY, RSGEO_FROM_KEY, RSHCR_FROM_KEY
     from STAGE.S_LOCATIONS_REGIONS
     order by KEY)
   loop
     if rLOC.UNSD_FROM_KEY is not null
     then
       iCount := iCount + 1;
-      P_LOCATION.INSERT_LOCATION_RELATIONSHIP(anLOC_ID(rLOC.UNSD_FROM_KEY), anLOC_ID(rLOC.KEY), 'UNSD');
+      P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+       (anLOC_ID(rLOC.UNSD_FROM_KEY), anLOC_ID(rLOC.KEY), 'UNSD', rLOC.START_DATE, rLOC.END_DATE);
+    --
+      INSERT_LOCATION_WITHIN
+       (anLOC_ID(rLOC.UNSD_FROM_KEY), anLOC_ID(rLOC.KEY), rLOC.START_DATE, rLOC.END_DATE);
     end if;
   --
     if rLOC.UNSDA_FROM_KEY is not null
     then
       iCount := iCount + 1;
-      P_LOCATION.INSERT_LOCATION_RELATIONSHIP(anLOC_ID(rLOC.UNSDA_FROM_KEY), anLOC_ID(rLOC.KEY), 'UNSDA');
+      P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+       (anLOC_ID(rLOC.UNSDA_FROM_KEY), anLOC_ID(rLOC.KEY), 'UNSDA', rLOC.START_DATE, rLOC.END_DATE);
+    --
+      INSERT_LOCATION_WITHIN
+       (anLOC_ID(rLOC.UNSDA_FROM_KEY), anLOC_ID(rLOC.KEY), rLOC.START_DATE, rLOC.END_DATE);
     end if;
   --
     if rLOC.HCRRESP_FROM_KEY is not null
     then
       iCount := iCount + 1;
-      P_LOCATION.INSERT_LOCATION_RELATIONSHIP(anLOC_ID(rLOC.HCRRESP_FROM_KEY), anLOC_ID(rLOC.KEY), 'HCRRESP');
+      P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+       (anLOC_ID(rLOC.HCRRESP_FROM_KEY), anLOC_ID(rLOC.KEY), 'HCRRESP', rLOC.START_DATE, rLOC.END_DATE);
     end if;
   --
     if rLOC.RSGEO_FROM_KEY is not null
     then
       iCount := iCount + 1;
-      P_LOCATION.INSERT_LOCATION_RELATIONSHIP(anLOC_ID(rLOC.RSGEO_FROM_KEY), anLOC_ID(rLOC.KEY), 'RSGEO');
+      P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+       (anLOC_ID(rLOC.RSGEO_FROM_KEY), anLOC_ID(rLOC.KEY), 'RSGEO', rLOC.START_DATE, rLOC.END_DATE);
     end if;
   --
     if rLOC.RSHCR_FROM_KEY is not null
     then
       iCount := iCount + 1;
-      P_LOCATION.INSERT_LOCATION_RELATIONSHIP(anLOC_ID(rLOC.RSHCR_FROM_KEY), anLOC_ID(rLOC.KEY), 'RSHCR');
+      P_LOCATION.INSERT_LOCATION_RELATIONSHIP
+       (anLOC_ID(rLOC.RSHCR_FROM_KEY), anLOC_ID(rLOC.KEY), 'RSHCR', rLOC.START_DATE, rLOC.END_DATE);
     end if;
   end loop;
 --
