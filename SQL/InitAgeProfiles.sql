e1ae2fd619c9a22e58efd5a64f42d7496da22ef0
Peter Scott <scott@unhcr.org>
2013-04-05 14:16:02 +0000

    Additional age profile STD1 used prior to 2006 (ranges 5-11 and 12-17 merged into a single range)

--- SQL/InitAgeProfiles.sql
+++ SQL/InitAgeProfiles.sql
@@ -8,5 +8,12 @@ begin
   P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '12-17 years', 'STD', 12, 17);
   P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '18-59 years', 'STD', 18, 59);
   P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '60 years or over', 'STD', 60, 999);
+--
+  P_AGE_PROFILE.INSERT_AGE_PROFILE('STD1', 'en', 'UNHCR standard age profile (prior to 2006)');
+--
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '0-4 years', 'STD1', 0, 4);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '5-17 years', 'STD1', 5, 17);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '18-59 years', 'STD1', 18, 59);
+  P_AGE_PROFILE.INSERT_AGE_RANGE(nAGR_ID, 'en', '60 years or over', 'STD1', 60, 999);
 end;
 /
