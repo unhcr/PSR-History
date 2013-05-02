96964f30834eedfcfc35aa6b9a506761723bcb1e
Peter Scott <scott@unhcr.org>
2013-05-02 09:02:10 +0000

    Initial import.

---
+++ SQL/S_ASR_T5_2002.sql
@@ -0,0 +1,70 @@
+create table S_ASR_T5_2002
+ (TABLE_NUMBER varchar2(5),
+  STATSYEAR varchar2(4),
+  DST_CODE varchar2(3),
+  COU_CODE_ASYLUM varchar2(3),
+  DIM_RSDTYPE_VALUE varchar2(1),
+  PROCEDURE_LEVEL varchar2(3),
+  APPLICATION_IND_GROUP varchar2(1),
+  APPLICATION_GROUP_SIZE varchar2(5),
+  DIM_RSDLEVEL_VALUE varchar2(2),
+  DECISION_IND_GROUP varchar2(1),
+  DECISION_GROUP_SIZE varchar2(5),
+  COU_CODE_ORIGIN varchar2(3),
+  ASY_START integer,
+  ASY_AH_START integer,
+  ASYAPP integer,
+  ASYREC_CV integer,
+  ASYREC_CP integer,
+  ASYREJ integer,
+  ASYOTHCL integer,
+  TOTAL_DECISIONS integer,
+  ASY_END integer,
+  ASY_AH_END integer)
+organization external
+ (type oracle_loader
+  default directory PSRDATA
+  access parameters
+   (records delimited by newline
+    characterset WE8MSWIN1252
+    badfile 'ASR_T5_2002.bad'
+    nodiscardfile
+    nologfile
+    skip 0
+    fields terminated by ','
+    optionally enclosed by '"' and '"'
+    lrtrim
+    missing field values are null
+     (FILLER1 char(4000),
+      FILLER2 char(4000),
+      FILLER3 char(4000),
+      FILLER4 char(4000),
+      FILLER5 char(4000),
+      APPLICATION_IND_GROUP char(4000),
+      DECISION_IND_GROUP char(4000),
+      COU_CODE_ASYLUM char(4000),
+      DIM_RSDTYPE_VALUE char(4000),
+      FILLER6 char(4000),
+      DIM_RSDLEVEL_VALUE char(4000),
+      COU_CODE_ORIGIN char(4000),
+      ASY_START char(4000),
+      ASY_AH_START char(4000),
+      ASYAPP char(4000),
+      ASYREC_CV char(4000),
+      ASYREC_CP char(4000),
+      ASYREJ char(4000),
+      ASYOTHCL char(4000),
+      TOTAL_DECISIONS char(4000),
+      ASY_END char(4000),
+      ASY_AH_END char(4000))
+    column transforms
+     (TABLE_NUMBER from constant '5',
+      STATSYEAR from constant '2002',
+      DST_CODE from constant 'ASY',
+      PROCEDURE_LEVEL from null,
+      APPLICATION_GROUP_SIZE from null,
+      DECISION_GROUP_SIZE from null))
+  location ('ASR_T5_2002.csv'))
+reject limit unlimited;
+
+grant select on S_ASR_T5_2002 to PSR_STAGE;
