7b0c48670bd4d10c74a4cd3d49a0c60f53a72b15
Peter Scott <scott@unhcr.org>
2013-05-02 11:50:10 +0000

    Numerous changes, particularly related to reorganisation of method of loading ASR data.

--- SQL/Rebuild.sql
+++ SQL/Rebuild.sql
@@ -5,6 +5,10 @@ spool Rebuild.log
 
 purge recyclebin;
 
+prompt StageSynonyms
+prompt =============
+@StageSynonyms
+
 prompt Schema
 prompt ======
 @Schema
@@ -13,13 +17,13 @@ prompt Views
 prompt =====
 @Views
 
-prompt MViews
-prompt ======
-@MViews
-
 prompt Packages
 prompt ========
 @Packages
+
+prompt MViews
+prompt ======
+@MViews
 
 prompt Triggers
 prompt ========
@@ -97,13 +101,45 @@ prompt LoadLocationsSubdivisions
 prompt =========================
 @LoadLocationsSubdivisions
 
+prompt LoadLocationRelationships
+prompt =========================
+@LoadLocationRelationships
+
 prompt LoadPPGs
 prompt ========
 @LoadPPGs
 
-prompt LoadStatistics
-prompt ==============
-@LoadStatistics
+--prompt LoadStatistics
+--prompt ==============
+--@LoadStatistics
+
+prompt LoadRefugeeStatistics
+prompt =====================
+@LoadRefugeeStatistics
+
+prompt LoadDemographicsStatistics
+prompt ==========================
+@LoadDemographicsStatistics
+
+prompt LoadRSDStatistics
+prompt =================
+@LoadRSDStatistics
+
+prompt LoadIDPStatistics
+prompt =================
+@LoadIDPStatistics
+
+prompt LoadReturnsStatistics
+prompt =====================
+@LoadReturnsStatistics
+
+prompt LoadStatelessStatistics
+prompt =======================
+@LoadStatelessStatistics
+
+prompt LoadOOCStatistics
+prompt =================
+@LoadOOCStatistics
 
 prompt DeactivateCodes
 prompt ===============
