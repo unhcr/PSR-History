ca350a57f3b558e10342fb8e0d7437e9c9475c82
Peter Scott <scott@unhcr.org>
2013-09-11 08:37:54 +0000

    Split out from Rebuild.sql.

---
+++ SQL/LoadData.sql
@@ -0,0 +1,43 @@
+prompt LoadRefugeeStatistics
+prompt =====================
+@LoadRefugeeStatistics
+
+prompt LoadDemographicsStatistics
+prompt ==========================
+@LoadDemographicsStatistics
+
+prompt LoadRSDStatistics
+prompt =================
+@LoadRSDStatistics
+
+prompt LoadIDPStatistics
+prompt =================
+@LoadIDPStatistics
+
+prompt LoadReturnsStatistics
+prompt =====================
+@LoadReturnsStatistics
+
+prompt LoadStatelessStatistics
+prompt =======================
+@LoadStatelessStatistics
+
+prompt LoadOOCStatistics
+prompt =================
+@LoadOOCStatistics
+
+prompt InitRedactionLimit
+prompt ==================
+@InitRedactionLimit
+
+prompt DeactivateCodes
+prompt ===============
+@DeactivateCodes
+
+prompt FixKosovoPPGs
+prompt =============
+@FixKosovoPPGs
+
+prompt RefreshMViews
+prompt =============
+@RefreshMViews.sql
