fa0274216fa55e1097d1b7bd432934f8840c9cf0
Peter Scott <scott@unhcr.org>
2013-09-11 08:37:33 +0000

    Split out from Rebuild.sql.

---
+++ SQL/BuildInfrastructure.sql
@@ -0,0 +1,115 @@
+prompt StageSynonyms
+prompt =============
+@StageSynonyms
+
+prompt Schema
+prompt ======
+@Schema
+
+--prompt Views
+--prompt =====
+--@Views
+
+prompt Packages
+prompt ========
+@Packages
+
+prompt Triggers
+prompt ========
+@Triggers
+
+prompt Context
+prompt =======
+@Context
+
+prompt PSQSchema
+prompt =========
+@PSQSchema
+
+prompt InitBase
+prompt ========
+@InitBase
+
+prompt LoadLanguages
+prompt =============
+@LoadLanguages
+
+prompt LoadTextTypes
+prompt =============
+@LoadTextTypes
+
+prompt LoadMessages
+prompt ============
+@LoadMessages
+
+prompt InitParameters
+prompt ==============
+@InitParameters
+
+prompt InitUsers
+prompt =========
+@InitUsers
+
+prompt LoadGenericCodes
+prompt ================
+@LoadGenericCodes
+
+prompt LoadDisplacementStatuses
+prompt ========================
+@LoadDisplacementStatuses
+
+prompt InitSexes
+prompt =========
+@InitSexes
+
+prompt InitAgeProfiles
+prompt ===============
+@InitAgeProfiles
+
+prompt LoadDimensionTypes
+prompt ==================
+@LoadDimensionTypes
+
+prompt LoadStatisticTypes
+prompt ==================
+@LoadStatisticTypes
+
+prompt LoadStatisticGroupAttributeTypes
+prompt =================================
+@LoadStatisticGroupAttributeTypes
+
+prompt LoadLocationTypes
+prompt =================
+@LoadLocationTypes
+
+prompt LoadLocationAttributeTypes
+prompt ==========================
+@LoadLocationAttributeTypes
+
+prompt LoadLocationRelationshipTypes
+prompt =============================
+@LoadLocationRelationshipTypes
+
+prompt LoadLocationsRegions
+prompt ====================
+@LoadLocationsRegions
+
+prompt LoadLocationsCountries
+prompt ======================
+@LoadLocationsCountries
+
+prompt LoadLocationTypeVariants
+prompt ========================
+@LoadLocationTypeVariants
+
+prompt LoadLocationsSubdivisions
+prompt =========================
+@LoadLocationsSubdivisions
+
+prompt LoadLocationRelationships
+prompt =========================
+@LoadLocationRelationships
+
+prompt LoadPPGs
+prompt ========
+@@LoadPPGs
