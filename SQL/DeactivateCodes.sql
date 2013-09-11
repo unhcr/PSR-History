45baa1c2d69ede078e7083b03a88bcb6f79bd021
Peter Scott <scott@unhcr.org>
2013-09-11 06:50:33 +0000

    Allow deactivation of location type variants.

--- SQL/DeactivateCodes.sql
+++ SQL/DeactivateCodes.sql
@@ -39,5 +39,37 @@ begin
   then dbms_output.put_line('1 DIMENSION_VALUES record inactivated');
   else dbms_output.put_line(to_char(iCount) || ' DIMENSION_VALUES records inactivated');
   end if;
+--
+  iCount := 0;
+  for rLOCTV in
+   (select LOCTV.ID as LOCTV_ID, LOCTV.VERSION_NBR
+    from STAGE.S_LOCATION_TYPE_VARIANTS STG
+    inner join COUNTRIES COU
+      on COU.UNHCR_COUNTRY_CODE = STG.HCRCC
+    inner join
+     (select LOCTV.ID, LOCTV.LOCT_CODE, LOCTV.LOC_ID, LOCTV.LOCRT_CODE, LOCTV.VERSION_NBR,
+        TXT.TEXT as DESCRIPTION
+      from T_LOCATION_TYPE_VARIANTS LOCTV
+      inner join T_TEXT_ITEMS TXT
+        on TXT.ITM_ID = LOCTV.ITM_ID
+        and TXT.TXTT_CODE = 'DESCR'
+        and TXT.SEQ_NBR = 1
+        and TXT.LANG_CODE = 'en') LOCTV
+      on LOCTV.LOCT_CODE = STG.LOCT_CODE
+      and LOCTV.LOC_ID = COU.ID
+      and LOCTV.LOCRT_CODE = STG.LOCRT_CODE
+      and LOCTV.DESCRIPTION = STG.DESCRIPTION_EN
+    where STG.ACTIVE_FLAG = 'N'
+    order by STG.HCRCC, STG.LOCT_CODE, STG.DESCRIPTION_EN)
+  loop
+    iCount := iCount + 1;
+    P_LOCATION.UPDATE_LOCATION_TYPE_VARIANT
+     (rLOCTV.LOCTV_ID, rLOCTV.VERSION_NBR, psACTIVE_FLAG => 'N');
+  end loop;
+--
+  if iCount = 1
+  then dbms_output.put_line('1 LOCATION_TYPE_VARIANTS record inactivated');
+  else dbms_output.put_line(to_char(iCount) || ' LOCATION_TYPE_VARIANTS records inactivated');
+  end if;
 end;
 /
