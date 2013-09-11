dfe7eac32edda8b375e84a14b01a8d13b42bdd18
Peter Scott <scott@unhcr.org>
2013-09-11 08:35:40 +0000

    Extensive changes for PSQ V2.

--- SQL/PSQSynonyms.sql
+++ SQL/PSQSynonyms.sql
@@ -1,4 +1,18 @@
+create synonym COUNTRY_SELECTION for &1..COUNTRY_SELECTION;
+create synonym ORIGIN_SELECTION for &1..ORIGIN_SELECTION;
+create synonym UNHCR_REGION_TREE for &1..UNHCR_REGION_TREE;
+create synonym UNSD_REGION_TREE for &1..UNSD_REGION_TREE;
+
+create synonym ASR_POC_DETAILS for &1..ASR_POC_DETAILS;
+create synonym ASR_POC_SUMMARY for &1..ASR_POC_SUMMARY;
+create synonym ASR_POC_YEARS for &1..ASR_POC_YEARS;
+
+create synonym ASR_DEMOGRAPHICS for &1..ASR_DEMOGRAPHICS;
 create synonym ASR_DEMOGRAPHICS_EN for &1..ASR_DEMOGRAPHICS_EN;
-create synonym ASR_POC_DETAILS_EN for &1..ASR_POC_DETAILS_EN;
-create synonym ASR_POC_SUMMARY_EN for &1..ASR_POC_SUMMARY_EN;
+create synonym ASR_DEMOGRAPHICS_FR for &1..ASR_DEMOGRAPHICS_FR;
+
+create synonym ASR_RSD for &1..ASR_RSD;
 create synonym ASR_RSD_EN for &1..ASR_RSD_EN;
+create synonym ASR_RSD_FR for &1..ASR_RSD_FR;
+
+create synonym P_CONTEXT for &1..P_CONTEXT;
