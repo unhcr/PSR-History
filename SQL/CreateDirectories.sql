2727c3de65f6860e0c47be86d0ff22bc18a6816b
Peter Scott <scott@unhcr.org>
2013-10-10 15:09:59 +0000

    Add creation of PSARCHIVE directory.

--- SQL/CreateDirectories.sql
+++ SQL/CreateDirectories.sql
@@ -3,3 +3,6 @@ grant read, write on directory PSRDATA to PSR_STAGE;
 
 create or replace directory PSRLOG as '/n01/RASRPRD/staging/log';
 grant read, write on directory PSRLOG to PSR_STAGE;
+
+create or replace directory PSRARCHIVE as '/n01/RASRPRD/staging/log';
+grant read, write on directory PSRARCHIVE to PSR_STAGE;
